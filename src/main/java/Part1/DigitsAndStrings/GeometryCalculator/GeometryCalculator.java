0f6d4bd2196e24ba0bf05ec834724a76b36a1e36
Roman GalkinÐÐ <garomait@gmail.com>
2022-06-03 20:47:29 +0400

    gk commit 1

--- src/main/java/Part1/DigitsAndStrings/GeometryCalculator/GeometryCalculator.java
+++ src/main/java/Part1/DigitsAndStrings/GeometryCalculator/GeometryCalculator.java
@@ -3,21 +3,38 @@ package Part1.DigitsAndStrings.GeometryCalculator;
 public class GeometryCalculator {
     // метод должен использовать абсолютное значение radius
     public static double getCircleSquare(double radius) {
-        return 0;
+
+        return Math.pow((Math.PI * radius), 2);
     }
 
     // метод должен использовать абсолютное значение radius
     public static double getSphereVolume(double radius) {
-        return 0;
+        return 4 / 3 * Math.PI * Math.pow(radius, 3);
     }
 
-    public static boolean isTriangleRightAngled(double a, double b, double c) {
-        return false;
+    public static double isTriangleRightAngled(double a, double b, double c) {
+
+        if (a + b > c) {
+            if (a + c > b) {
+                if (b + c > a) {
+                    System.out.println("Треугольник существует");
+                }
+            }
+        } else {
+            return -1.0;
+        }
+
+        return 1.0;
     }
 
     // перед расчетом площади рекомендуется проверить возможен ли такой треугольник
     // методом isTriangleRightAngled, если невозможен вернуть -1.0
     public static double getTriangleSquare(double a, double b, double c) {
-        return 0;
+
+        double p = (a + b + c) / 2;
+
+        double s = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+
+        return s;
     }
 }
