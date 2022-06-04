f9cb97cb7f20796c5f9e00ef22684d0a310d7122
Roman GalkinÐÐ <garomait@gmail.com>
2022-06-04 13:11:35 +0400

    gk commit 2

--- src/main/java/Part1/DigitsAndStrings/GeometryCalculator/GeometryCalculator.java
+++ src/main/java/Part1/DigitsAndStrings/GeometryCalculator/GeometryCalculator.java
@@ -12,29 +12,31 @@ public class GeometryCalculator {
         return 4 / 3 * Math.PI * Math.pow(radius, 3);
     }
 
-    public static double isTriangleRightAngled(double a, double b, double c) {
+    public static boolean isTriangleRightAngled(double a, double b, double c) {
 
         if (a + b > c) {
             if (a + c > b) {
                 if (b + c > a) {
-                    System.out.println("Треугольник существует");
                 }
             }
+            return true;
         } else {
-            return -1.0;
+            return false;
         }
-
-        return 1.0;
     }
 
     // перед расчетом площади рекомендуется проверить возможен ли такой треугольник
     // методом isTriangleRightAngled, если невозможен вернуть -1.0
     public static double getTriangleSquare(double a, double b, double c) {
 
-        double p = (a + b + c) / 2;
+        if (isTriangleRightAngled(a, b, c)) {
 
-        double s = Math.sqrt(p * (p - a) * (p - b) * (p - c));
-
-        return s;
+            double p = (a + b + c) / 2;
+            double s = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+            return s;
+        } else {
+            return -1.0;
+        }
     }
 }
+
