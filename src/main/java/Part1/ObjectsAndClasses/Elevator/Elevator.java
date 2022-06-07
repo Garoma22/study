bb7d1bc110068290aea0b8ddc14f92e7b8ef7345
Roman GalkinÐÐ <garomait@gmail.com>
2022-06-07 18:25:53 +0400

    elevator_commit_1

--- src/main/java/Part1/ObjectsAndClasses/Elevator/Elevator.java
+++ src/main/java/Part1/ObjectsAndClasses/Elevator/Elevator.java
@@ -1,4 +1,63 @@
 package Part1.ObjectsAndClasses.Elevator;
 
 public class Elevator {
+
+    private int currentFloor = 1;
+    private final int minFloor;
+    private final int maxFloor;
+
+    public Elevator(int minFloor, int maxFloor) {
+        this.minFloor = minFloor;
+        this.maxFloor = maxFloor;
+    }
+
+//    public int getCurrentFloor() {
+//        return currentFloor;
+//    }
+
+    public void moveDown() {
+        currentFloor--;
+
+    }
+
+    public void moveUp() {
+        currentFloor++;
+
+    }
+
+    public void move(int floor) {
+                if (floor >= minFloor && floor <= maxFloor && currentFloor < floor) {
+
+            while (floor != currentFloor) {
+
+                moveUp();
+
+                System.out.println("Этаж " + currentFloor);
+
+            }
+            System.out.println("Мы поднялись на нужный этаж " + currentFloor);
+            return;
+        }
+
+        if (floor >= minFloor && floor <= maxFloor && currentFloor > floor) {
+
+            while (floor != currentFloor) {
+
+                moveDown();
+                System.out.println("Этаж " + currentFloor);
+
+            }
+            System.out.println("Мы опустились на нужный этаж " + currentFloor);
+            return;
+        }
+
+        if (floor < minFloor || floor > maxFloor) {
+            System.out.println("Некорректный ввод этажа");
+        } else {
+
+            System.out.println("Мы находимся на " + currentFloor + " этаже");
+        }
+
+    }
 }
+
