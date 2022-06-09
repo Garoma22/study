443a8b10cfc0c3110448386130d981624f7b52b9
Roman GalkinÐÐ <garomait@gmail.com>
2022-06-09 23:19:48 +0400

    calculator_commit_1

--- src/main/java/Part1/ObjectsAndClasses/Calculator/Calculator.java
+++ src/main/java/Part1/ObjectsAndClasses/Calculator/Calculator.java
@@ -1,4 +1,19 @@
 package Part1.ObjectsAndClasses.Calculator;
 
 public class Calculator {
+
+    int calculate(){
+    return Operation.ADD.n1 + Operation.ADD.n2;
+    }
+
+    int calculate2(){
+        return Operation.SUBTRACT.n1/ Operation.SUBTRACT.n2;
+    }
+
+    int calculate3(){
+        return Operation.MULTIPLY.n1* Operation.MULTIPLY.n2;
+    }
 }
+
+
+
--- src/main/java/Part1/ObjectsAndClasses/Calculator/Main.java
+++ src/main/java/Part1/ObjectsAndClasses/Calculator/Main.java
@@ -3,5 +3,12 @@ package Part1.ObjectsAndClasses.Calculator;
 public class Main {
     public static void main(String[] args) {
 
+        Calculator calculator  = new Calculator();
+
+        System.out.println(calculator.calculate());
+        System.out.println(calculator.calculate2());
+        System.out.println(calculator.calculate3());
+
+
     }
 }
