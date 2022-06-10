e81d1e4c5d610e3c7e5c35a4aa8ef67ecf6c541d
Roman GalkinÐÐ <garomait@gmail.com>
2022-06-10 20:12:13 +0400

    calculator_commit_2

--- src/main/java/Part1/ObjectsAndClasses/Elevator/Elevator.java
+++ src/main/java/Part1/ObjectsAndClasses/Elevator/Elevator.java
@@ -10,54 +10,34 @@ public class Elevator {
         this.minFloor = minFloor;
         this.maxFloor = maxFloor;
     }
-
-//    public int getCurrentFloor() {
-//        return currentFloor;
-//    }
-
     public void moveDown() {
         currentFloor--;
-
     }
-
     public void moveUp() {
         currentFloor++;
-
     }
-
     public void move(int floor) {
-                if (floor >= minFloor && floor <= maxFloor && currentFloor < floor) {
-
+        if (floor >= minFloor && floor <= maxFloor && currentFloor < floor) {
             while (floor != currentFloor) {
-
                 moveUp();
-
                 System.out.println("Этаж " + currentFloor);
-
             }
             System.out.println("Мы поднялись на нужный этаж " + currentFloor);
             return;
         }
-
         if (floor >= minFloor && floor <= maxFloor && currentFloor > floor) {
-
             while (floor != currentFloor) {
-
                 moveDown();
                 System.out.println("Этаж " + currentFloor);
-
             }
             System.out.println("Мы опустились на нужный этаж " + currentFloor);
             return;
         }
-
         if (floor < minFloor || floor > maxFloor) {
             System.out.println("Некорректный ввод этажа");
         } else {
-
             System.out.println("Мы находимся на " + currentFloor + " этаже");
         }
-
     }
 }
 
