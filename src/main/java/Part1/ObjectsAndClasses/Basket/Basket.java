5e8faa4c29cf424b03bc33802dbda9cb356fde12
Roman GalkinÐÐ <garomait@gmail.com>
2022-06-25 18:01:52 +0400

    basket_commit_3

--- src/main/java/Part1/ObjectsAndClasses/Basket/Basket.java
+++ src/main/java/Part1/ObjectsAndClasses/Basket/Basket.java
@@ -3,11 +3,16 @@ package Part1.ObjectsAndClasses.Basket;
 public class Basket {
 
     private String items;
-    private double totalWeight = 0;   // общая масса товаров, добавленных в корзину
-    private int totalPrice = 0; // общая стоимость товаров
+    private double totalWeight;   // общая масса товаров, добавленных в корзину
+    private double totalPrice; // общая стоимость товаров
+    private static double allWeight;
+    private static double allPrice;
+    private static int countPositions;
+    private static int countBaskets;
 
     public Basket() {
         items = "Список товаров:";
+        countBaskets++;
     }
 
     public Basket(String items) {
@@ -26,7 +31,7 @@ public class Basket {
         add(name, price, 1);
     }
 
-    public void add(String name, int price, int count) {
+    public void add(String name, double price, int count) {
         boolean error = false;
         if (items.contains(name)) {
             error = true;
@@ -38,26 +43,29 @@ public class Basket {
         }
 
         items = items + "\n" + name + " - " +
-            count + " шт. - " + price;
+                count + " шт. - " + price + " руб";
 
         totalPrice = totalPrice + price;  //добавил расчет стоимости всего
+        countPositions = countPositions + count;
     }
 
     // дополнительный метод
-    public void add(String name, int price, int count, double weight){
+    public void add(String name, int price, int count, double weight) {
 
-        add(name,price,count);
+        add(name, price, count);
 
         totalWeight = totalWeight + weight;
-        }
 
-        public void getTotalWeight(){
-            System.out.println("Общий вес продуктов : " + totalWeight);
-        }
+    }
 
-        public void getTotalPrice(){
-            System.out.println("Общая цена продуктов : " + totalPrice);
-        }
+    public void getTotalWeight() {
+        System.out.println("Общий вес продуктов в корзинке : " +  totalWeight);
+    }
+
+    public void getTotalPrice() {
+
+        System.out.println("Общая цена продуктов в корзине : " + totalPrice);
+    }
 
     public void clear() {
         items = "";
@@ -71,4 +79,48 @@ public class Basket {
             System.out.println(items);
         }
     }
+
+    // ПЕРЕГРУЗКА МЕТОДА ДЯЛ ТРЕХ КОЗИНОК
+    public static void findAllPrice(Basket b2) {
+        allPrice = allPrice + b2.totalPrice;
+        System.out.println("TOTAL Price в " + countBaskets + " корзинках : " + allPrice + " руб ");
+    }
+    public static void findAllPrice(Basket basket, Basket basket1) {
+        allPrice = allPrice + basket.totalPrice + basket1.totalPrice;
+        System.out.println("TOTAL Price в " + countBaskets + " корзинках : " + allPrice + " руб ");
+    }
+
+
+    // ПЕРЕГРУЗКА МЕТОДА ДЛЯ ТРЕХ КОРЗИНОК
+    public static void findAllWeight(Basket b2) {
+        allWeight = allWeight + b2.totalWeight;
+        System.out.println("TOTAL weight в " + countBaskets + " корзинках : " + allWeight + " грамм ");
+
+    }
+    public static void findAllWeight(Basket basket, Basket basket1) {
+        allWeight = allWeight + basket1.totalWeight + basket.totalWeight;
+        System.out.println("TOTAL weight в " + countBaskets + " корзинках : " + allWeight + " грамм ");
+    }
+
+    public static void findAveragePriceGoods() {
+        System.out.println();
+        double averagePrice = allPrice / countPositions;
+        System.out.print("РАСЧЕТ СРЕДНЕЙ ЦЕНЫ ТОВАРА В " + countBaskets + " КОРЗИНАХ : \n" +
+                "Общая цена позиций : " + allPrice + "\nЧисло позиций : " + countPositions + "\n" + "ИТОГО средняя " +
+                "цена одной позиции : ");
+
+        double scale = Math.pow(10, 3);
+        averagePrice = Math.ceil(averagePrice * scale) / scale;
+        System.out.print(averagePrice);
+
+    }
+    public static void findAverageBasketPrice() {
+        System.out.println();
+        System.out.println();
+        System.out.println("РАСЧЕТ СРЕДЕНЙ ЦЕНЫ 1-й КОРЗИНКИ (ВЫБОРКА СРЕДИ " + countBaskets + "-X КОРЗИНОК)");
+
+        double averageBasketPrice = allPrice / countBaskets;
+        System.out.println("Средняя цена корзинки : " + averageBasketPrice);
+    }
 }
+
--- src/main/java/Part1/ObjectsAndClasses/Basket/Main.java
+++ src/main/java/Part1/ObjectsAndClasses/Basket/Main.java
@@ -4,27 +4,78 @@ public class Main {
 
     public static void main(String[] args) {
         Basket basket = new Basket();
+        Basket basket1 = new Basket();
 
 
-        basket.add("BigMac",760,3,400);
-        basket.add("Zewa", 220,12, 270);
+        basket.add("BigMac", 760, 3, 400);
+        basket.add("Zewa", 220, 12, 275);
         basket.add("Netflix", 550);
-        basket.add("Лотерейные билеты", 1100,11);
+        basket.add("Лотерейные билеты", 1100, 11);
 
+
+        basket.print("__КОРЗИНА 1__");
         basket.getTotalPrice();
         basket.getTotalWeight();
-        basket.print("Zewa");
+        System.out.println();
+
+        basket1.add("Macflury", 260, 3, 200);
+        basket1.add("Coca-cola", 200, 1, 1505);
+        basket1.add("Билеты на Бетмена", 900, 2);
+        basket1.add("Качан капусты", 40, 1, 420);
+
+        basket1.print("__КОРЗИНА 2__");
+        basket1.getTotalPrice();
+        basket1.getTotalWeight();
+        System.out.println();
+
+        System.out.println("РАСЧИТЫВАЕМ ЦЕНУ И ВЕС В 2-Х КОРЗИНКАХ : ");
+        System.out.println();
+
+        // ОБЩАЯ ЦЕНА И ВЕС ВСЕХ ОБОИХ КОРЗИН
+        Basket.findAllPrice(basket, basket1);
+        Basket.findAllWeight(basket, basket1);
 
 
+        // СРЕДНЯЯ ЦЕНА ВСЕХ ПОЗИЦИЙ В ОБОИХ КОРЗИНАХ
+        Basket.findAveragePriceGoods();
+        System.out.println();
+
+        Basket.findAverageBasketPrice();
+        System.out.println();
 
 
-
-//        basket.add("Milk", 40);
-//        basket.print("Milk");
+        System.out.println("ДОБАВИЛИ 3-Ю КОРЗИНКУ");
 
 
+        // ДОБАВИЛИ ТРЕТЬЮ КОРЗИНКУ И ПЕРЕГУЗИЛИ МЕТОДЫ
+        Basket basket2 = new Basket();
+
+        System.out.println();
 
 
+        basket2.add("Водка ПУТИНКА", 333, 1, 532);
+        basket2.add("Шоколадка MARS", 80, 1, 80);
+
+        basket2.print("__КОРЗИНКА 3__");
+        System.out.println();
+
+        System.out.println("ОБЩАЯ ЦЕНА И ВЕС 3-Х КОРЗИНОК : ");
+        System.out.println();
+
+        Basket.findAllPrice(basket2);
+        Basket.findAllWeight( basket2);
+
+
+        System.out.println();
+
+        //ИЩЕМ СРЕДНЮЮ ЦЕНУ В ПОЗИЦИИИ СРЕДИ ТРЕХ КОРЗИНОК
+
+        Basket.findAveragePriceGoods();
+        System.out.println();
+
+        //РАСЧЕТ СРЕДНЕЙ ЦЕНЫ 1-Й КОРЗИНКИ (ВЫБРОКА СРЕДИ 3 КОРЗИНОК)
+        Basket.findAverageBasketPrice();
 
     }
+
 }
