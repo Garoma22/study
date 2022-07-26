eeaa547b950562a9e65f71df9f1d2ed6bae9a44d
Roman GalkinÐÐ <garomait@gmail.com>
2022-07-26 18:31:44 +0400

    digits_strings_1.2

--- src/main/java/Part1/DigitsAndStrings/StringExperiments/Task1.java
+++ src/main/java/Part1/DigitsAndStrings/StringExperiments/Task1.java
@@ -1,10 +1,23 @@
 package Part1.DigitsAndStrings.StringExperiments;
 
+import java.nio.charset.Charset;
+import java.util.Locale;
+import java.util.SortedMap;
+
 public class Task1 {
     public static void main(String[] args) {
-
-
-
-
+        String literals = "abcdefghijklmnopqrstuvwxyz";
+        literals = literals.toUpperCase(Locale.ROOT);
+        abc(literals);
+    }
+    static void abc(String l) {
+        char[] chars = l.toCharArray();
+        for (char c : chars){
+            System.out.println( c + " : " + Character.getNumericValue(c));
+        }
+        System.out.println("____________");
+        for (char c : chars){
+            System.out.println( c + " : " + Character.getName(c));
+        }
     }
 }
