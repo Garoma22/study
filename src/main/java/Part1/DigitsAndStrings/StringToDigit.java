49ac78e59ef01ee1b6cbbedcb43d9e7dc498fe0d
Roman GalkinÐÐ <garomait@gmail.com>
2022-07-26 18:21:58 +0400

    digits_strings_1

--- src/main/java/Part1/DigitsAndStrings/StringToDigit.java
+++ src/main/java/Part1/DigitsAndStrings/StringToDigit.java
@@ -1,7 +1,28 @@
 package Part1.DigitsAndStrings;
 
+import java.nio.charset.Charset;
+import java.util.Locale;
+import java.util.SortedMap;
+
 public class StringToDigit {
     public static void main(String[] args) {
-
+        String literals = "abcdefghijklmnopqrstuvwxyz";
+        literals = literals.toUpperCase(Locale.ROOT);
+        abc(literals);
+    }
+    static void abc(String l) {
+        char[] chars = l.toCharArray();
+        for (char c : chars){
+            System.out.println( c + " : " + Character.getNumericValue(c));
+        }
+        System.out.println("____________");
+        for (char c : chars){
+            System.out.println( c + " : " + Character.getName(c));
+        }
     }
 }
+
+
+
+
+
