d15cef6a1d0f943c0b5fd6b8276c59d7dc012cbe
Roman GalkinÐÐ <garomait@gmail.com>
2022-08-13 23:01:40 +0400

    Part_2_Task_2_commit_1

--- src/main/java/Part2/Hospital/Hospital.java
+++ src/main/java/Part2/Hospital/Hospital.java
@@ -2,23 +2,48 @@ package Part2.Hospital;
 
 public class Hospital {
 
+    public static final int MAX_TEMP = 40;
+    public static final int MIN_TEMP = 32;
+    public static final float MIN_TEMP_FOR_HEALTH = 36.2f;
+    public static final float MAX_TEMP_FOR_HEALTH = 36.9f;
+
     public static float[] generatePatientsTemperatures(int patientsCount) {
-        return new float[0];
+        float [] arr = new float[patientsCount];
+        for (int i = 0; i < arr.length; i++) {
+            int diff = MAX_TEMP - MIN_TEMP;
+            arr[i] = (float) (Math.random() * diff) + 32;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            float scale = (float) Math.pow(10,1);
+            arr[i] = (float) (Math.ceil(arr[i] * scale) / scale);
+        }
+        return arr;
     }
-
     public static String getReport(float[] temperatureData) {
-        /*
-        TODO: Напишите код, который выводит среднюю температуру по больнице,количество здоровых пациентов,
-            а также температуры всех пациентов.
-            Округлите среднюю температуру с помощью Math.round до 2 знаков после запятой,
-            а температуры каждого пациента до 1 знака после запятой
-        */
 
-        String report =
-            "Температуры пациентов: " + 1 +
-            "\nСредняя температура: " + 0 +
-            "\nКоличество здоровых: " + 0;
+        StringBuilder sb = new StringBuilder();
+        for (float temperatureDatum : temperatureData) {
+            sb.append(temperatureDatum).append(" ");
+        }
+        float sum = 0;
+        for (float temperatureDatum : temperatureData) {
+            sum += temperatureDatum;
+        }
+        float midTemp = sum/temperatureData.length;
+        float scale = (float) Math.pow(10,2);
+        float midTempTrim = (float) (Math.ceil(midTemp * scale)/scale);
 
-        return report;
+        int countHealthy  = 0;
+
+        for (float temperatureDatum : temperatureData) {
+            if (temperatureDatum >= MIN_TEMP_FOR_HEALTH
+                    && temperatureDatum <= MAX_TEMP_FOR_HEALTH) {
+                countHealthy++;
+            }
+        }
+
+        return "Температуры пациентов: " + sb +
+        "\nСредняя температура: " + midTempTrim +
+        "\nКоличество здоровых: " + countHealthy;
     }
 }
