4e623086108c4bfd8c28ddf5a957320dd0ae9c4a
Roman GalkinÐÐ <garomait@gmail.com>
2022-08-27 23:44:55 +0400

    Part_2_Task_2(Hospital)_commit_2

--- src/main/java/Part2/Hospital/Hospital.java
+++ src/main/java/Part2/Hospital/Hospital.java
@@ -7,43 +7,39 @@ public class Hospital {
     public static final float MIN_TEMP_FOR_HEALTH = 36.2f;
     public static final float MAX_TEMP_FOR_HEALTH = 36.9f;
 
+    private static int sumOfTemperatures = 0;
+    private static int countHealthy = 0;
+
+
     public static float[] generatePatientsTemperatures(int patientsCount) {
-        float [] arr = new float[patientsCount];
+        float[] arr = new float[patientsCount];
+
         for (int i = 0; i < arr.length; i++) {
             int diff = MAX_TEMP - MIN_TEMP;
             arr[i] = (float) (Math.random() * diff) + 32;
-        }
-        for (int i = 0; i < arr.length; i++) {
-            float scale = (float) Math.pow(10,1);
+            float scale = (float) Math.pow(10, 1);
             arr[i] = (float) (Math.ceil(arr[i] * scale) / scale);
-        }
-        return arr;
-    }
-    public static String getReport(float[] temperatureData) {
+            sumOfTemperatures += arr[i];
 
-        StringBuilder sb = new StringBuilder();
-        for (float temperatureDatum : temperatureData) {
-            sb.append(temperatureDatum).append(" ");
-        }
-        float sum = 0;
-        for (float temperatureDatum : temperatureData) {
-            sum += temperatureDatum;
-        }
-        float midTemp = sum/temperatureData.length;
-        float scale = (float) Math.pow(10,2);
-        float midTempTrim = (float) (Math.ceil(midTemp * scale)/scale);
-
-        int countHealthy  = 0;
-
-        for (float temperatureDatum : temperatureData) {
-            if (temperatureDatum >= MIN_TEMP_FOR_HEALTH
-                    && temperatureDatum <= MAX_TEMP_FOR_HEALTH) {
+            if (arr[i] >= MIN_TEMP_FOR_HEALTH
+                    && arr[i] <= MAX_TEMP_FOR_HEALTH) {
                 countHealthy++;
             }
         }
+        return arr;
+    }
+
+    public static String getReport(float[] temperatureData) {
+        StringBuilder sb = new StringBuilder();
+        for (float temperaturePerson : temperatureData) {
+            sb.append(temperaturePerson).append(" ");
+        }
+        float midTemp = (float) sumOfTemperatures / temperatureData.length;
+        float scale = (float) Math.pow(10, 2);
+        float midTempTrim = (float) (Math.ceil(midTemp * scale) / scale);
 
         return "Температуры пациентов: " + sb +
-        "\nСредняя температура: " + midTempTrim +
-        "\nКоличество здоровых: " + countHealthy;
+                "\nСредняя температура: " + midTempTrim +
+                "\nКоличество здоровых: " + countHealthy;
     }
 }
