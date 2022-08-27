459aaa6be090676267df5e9c7f6f7082bfb9d074
Roman GalkinÐÐ <garomait@gmail.com>
2022-08-28 00:46:18 +0400

    Part_2_Task_4(TodoList)_commit_1

--- src/main/java/Part2/TodoList/Main.java
+++ src/main/java/Part2/TodoList/Main.java
@@ -1,9 +1,21 @@
 package Part2.TodoList;
 
+import java.util.regex.Pattern;
+
 public class Main {
     private static TodoList todoList = new TodoList();
 
     public static void main(String[] args) {
-        // TODO: написать консольное приложение для работы со списком дел todoList
+
+
+        // Создать регулярное выражение из строки "LIST,ADD,EDIT,DELETE"
+
+        String s = "LIST,ADD,EDIT,DELETE";
+
+        Pattern p = Pattern.compile(s);
+
+
+
+
     }
 }
