99ae06332591f6a586a730d535aabb4672748080
Roman GalkinÐÐ <garomait@gmail.com>
2022-09-03 01:44:51 +0400

    Part_2_Task_4(TodoList)_commit_3

--- src/main/java/Part2/TodoList/Main.java
+++ src/main/java/Part2/TodoList/Main.java
@@ -1,66 +1,49 @@
 package Part2.TodoList;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
     private static final TodoList todoList = new TodoList();
-
     public static void main(String[] args) {
-
         while (true) {
-            Scanner scanner = new Scanner(System.in);
             System.out.println("Введите команду: ");
+            Scanner scanner = new Scanner(System.in);
+            String ss = String.valueOf(scanner.nextLine());
 
-            String line = scanner.nextLine();
-            switch (line) {
-                case "LIST":
-                    if(todoList.getTodos().size() == 0){
-                        System.out.println("Список пустой");
-                        continue;
-                    }
-                //    todoList.getTodos();
-                    todoList.showList();
-                    break;
-                case "ADD":
-                    System.out.println("Сделайте запись в todoList : ");
-                    String appointment1 = scanner.nextLine();
-                    todoList.add(appointment1);
-                    break;
-                case "ADD2":
-                    System.out.println("Укажите номер ячейки,куда вставить запись : ");
-                    int numForAdd = Integer.parseInt(scanner.nextLine()) - 1 ; // единица нужна чтобы не было нулевого дела в списке
-                    System.out.println("Сделайте запись в todoList : ");
-                    String appointment3 = scanner.nextLine();
-                    todoList.insert(numForAdd,appointment3);
-                    break;
-                case "EDIT":
-                    System.out.println("Укажите номер ячейки на замену : ");
-                    int numForEdit = Integer.parseInt(scanner.nextLine()) - 1 ; // единица нужна чтобы не было нулевого дела в списке
-                    System.out.println("Сделайте запись в todoList : ");
-                    String appointment4 = scanner.nextLine();
-                    if (numForEdit>todoList.getSchedule().size()){
-                        System.out.println("Такой ячейки пока нет");
-                        continue;
-                    }
-                    todoList.edit(numForEdit,appointment4);
-                    break;
-                case "DELETE":
-                    System.out.println("Укажите номер ячейки на удаление : ");
-                        int numForDel = Integer.parseInt(scanner.nextLine()) - 1 ;
-                        todoList.delete(numForDel);
-                    break;
-                default:
-                    System.out.println("МИМО");
+            String listing = "L?l?I?i?S?s?T?t?";
+            String adding = "[a-dA-D]{3}";
+            String addOnIndex = "[a-dA-D]{3}\\s+\\d+";
+            String editing = "E?e?D?d?I?i?T?t?\\s+\\d+";
+            String deleting = "D?d?E?e?L?l?E?e?T?t?E?e?\\s+\\d+";
+
+            if (ss.matches(listing)) {
+                todoList.showList();
+            }
+            if (ss.matches(adding)) {
+                System.out.println("Сделайте запись : ");
+                todoList.add(scanner.nextLine());
+            }
+            if (ss.matches(addOnIndex)) {      // выковыриваем цифру индекса из строки
+                int num = todoList.findNum(ss);
+                System.out.println("Сделайте запись (индекс уже определен) : ");
+                todoList.insert(num, scanner.nextLine());
+            }
+            if (ss.matches(editing)){
+                if (todoList.getSchedule().size()==0){
+                    System.out.println("Лист пустой");
+                    continue;
+                }
+                int num2 = todoList.findNum(ss);
+                System.out.println("Замените пункт (индекс уже определен) : ");
+                todoList.edit(num2,scanner.nextLine());
+            }
+            if(ss.matches(deleting)){
+                int num3 = todoList.findNum(ss);
+                System.out.println("Удаляем пункт (индекс уже определен) :");
+                todoList.delete(num3);
             }
         }
     }
 }
-
-
-
--- src/main/java/Part2/TodoList/TodoList.java
+++ src/main/java/Part2/TodoList/TodoList.java
@@ -1,44 +1,43 @@
 package Part2.TodoList;
 
-import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class TodoList {
-
     private final List<String> schedule = new ArrayList<>();
-
-    public List<String> getSchedule() {
-        return schedule;
-    }
-
     public void add(String todo) {
         schedule.add(todo);
+        showList();
     }
 
     public void insert(int index, String todo) {
         if (schedule.size() + 1 >= index) {
             schedule.add(index, todo);
+            showList();
         } else {
-            System.out.println("Вставка невозможна, лист короткий");
+            schedule.add(todo);
+            showList();
         }
     }
     public void edit(int index, String todo) {
-        schedule.remove(index);
-        schedule.add(index, todo);
-    }
 
+        if (schedule.size() + 1 >= index && schedule.size()!= 0) {
+            schedule.remove(index);
+            schedule.add(index, todo);
+        }else{
+            System.out.println("Редактировать нечего или такого индекса нет");
+        }
+        showList();
+    }
     public void delete(int index) {
         if (index <= schedule.size() + 1) {
-            schedule.remove(index);}
-        else {
+            schedule.remove(index);
+            showList();
+        } else {
             System.out.println("Такого индекса нет");
         }
     }
-    public List<String> getTodos() {
-        System.out.println(schedule);
-        return schedule;
-    }
+
     public void showList() {
         int counter = 0;
         System.out.println("TodoList :");
@@ -46,6 +45,32 @@ public class TodoList {
             counter++;
             System.out.println(counter + ". " + s);
         }
+    }
+    public int findNum(String ss) {  //EDIT 3
+        char[] arr1 = ss.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (char c : arr1) {
+            if (c == '0' ||
+                    c == '1' ||
+                    c == '2' ||
+                    c == '3' ||
+                    c == '4' ||
+                    c == '5' ||
+                    c == '6' ||
+                    c == '7' ||
+                    c == '8' ||
+                    c == '9') {
+                sb.append(c);
+            }
+        }
+        int num = Integer.parseInt(String.valueOf(sb)) - 1;
+        if (num < 0) {
+            num = 0;
+        }
+        return num;
+    }
+    public List<String> getSchedule() {
+        return schedule;
     }
 }
 
