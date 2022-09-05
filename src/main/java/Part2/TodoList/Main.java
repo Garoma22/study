cb4e63e293b82b442503da7511ad2b0f2e3519c1
Roman GalkinÐÐ <garomait@gmail.com>
2022-09-05 19:59:48 +0400

    Part_2_Task_4(TodoList)_commit_4

--- src/main/java/Part2/TodoList/Main.java
+++ src/main/java/Part2/TodoList/Main.java
@@ -1,49 +1,18 @@
 package Part2.TodoList;
 
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Main {
-    private static final TodoList todoList = new TodoList();
+    static final TodoList todoList = new TodoList();
+
     public static void main(String[] args) {
-        while (true) {
-            System.out.println("Введите команду: ");
-            Scanner scanner = new Scanner(System.in);
-            String ss = String.valueOf(scanner.nextLine());
-
-            String listing = "L?l?I?i?S?s?T?t?";
-            String adding = "[a-dA-D]{3}";
-            String addOnIndex = "[a-dA-D]{3}\\s+\\d+";
-            String editing = "E?e?D?d?I?i?T?t?\\s+\\d+";
-            String deleting = "D?d?E?e?L?l?E?e?T?t?E?e?\\s+\\d+";
-
-            if (ss.matches(listing)) {
-                todoList.showList();
-            }
-            if (ss.matches(adding)) {
-                System.out.println("Сделайте запись : ");
-                todoList.add(scanner.nextLine());
-            }
-            if (ss.matches(addOnIndex)) {      // выковыриваем цифру индекса из строки
-                int num = todoList.findNum(ss);
-                System.out.println("Сделайте запись (индекс уже определен) : ");
-                todoList.insert(num, scanner.nextLine());
-            }
-            if (ss.matches(editing)){
-                if (todoList.getSchedule().size()==0){
-                    System.out.println("Лист пустой");
-                    continue;
-                }
-                int num2 = todoList.findNum(ss);
-                System.out.println("Замените пункт (индекс уже определен) : ");
-                todoList.edit(num2,scanner.nextLine());
-            }
-            if(ss.matches(deleting)){
-                int num3 = todoList.findNum(ss);
-                System.out.println("Удаляем пункт (индекс уже определен) :");
-                todoList.delete(num3);
-            }
-        }
+        todoList.checkRegex();
     }
 }
+
+
+
+
+
+
+
+
+
--- src/main/java/Part2/TodoList/TodoList.java
+++ src/main/java/Part2/TodoList/TodoList.java
@@ -2,42 +2,39 @@ package Part2.TodoList;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
+import static Part2.TodoList.Main.todoList;
 
 public class TodoList {
     private final List<String> schedule = new ArrayList<>();
+
     public void add(String todo) {
         schedule.add(todo);
-        showList();
     }
 
     public void insert(int index, String todo) {
-        if (schedule.size() + 1 >= index) {
-            schedule.add(index, todo);
-            showList();
+        if (index >= 1 && index <= schedule.size()) {
+            schedule.add(index -1, todo);
         } else {
             schedule.add(todo);
-            showList();
         }
     }
     public void edit(int index, String todo) {
-
-        if (schedule.size() + 1 >= index && schedule.size()!= 0) {
-            schedule.remove(index);
-            schedule.add(index, todo);
-        }else{
+        if (index >= 1 && index <= schedule.size()) {
+            schedule.remove(index - 1);
+            schedule.add(index-1, todo);
+        } else {
             System.out.println("Редактировать нечего или такого индекса нет");
         }
-        showList();
     }
     public void delete(int index) {
-        if (index <= schedule.size() + 1) {
-            schedule.remove(index);
-            showList();
+        if (index >= 1 && index <= schedule.size()) {
+            schedule.remove(index - 1);
         } else {
             System.out.println("Такого индекса нет");
         }
     }
-
     public void showList() {
         int counter = 0;
         System.out.println("TodoList :");
@@ -46,33 +43,57 @@ public class TodoList {
             System.out.println(counter + ". " + s);
         }
     }
-    public int findNum(String ss) {  //EDIT 3
-        char[] arr1 = ss.toCharArray();
-        StringBuilder sb = new StringBuilder();
-        for (char c : arr1) {
-            if (c == '0' ||
-                    c == '1' ||
-                    c == '2' ||
-                    c == '3' ||
-                    c == '4' ||
-                    c == '5' ||
-                    c == '6' ||
-                    c == '7' ||
-                    c == '8' ||
-                    c == '9') {
-                sb.append(c);
-            }
-        }
-        int num = Integer.parseInt(String.valueOf(sb)) - 1;
-        if (num < 0) {
-            num = 0;
-        }
-        return num;
-    }
     public List<String> getSchedule() {
         return schedule;
     }
+    public void checkRegex() {
+        while (true) {
+            System.out.println("Введите команду: ");
+            Scanner scanner = new Scanner(System.in);
+            String ss = String.valueOf(scanner.nextLine());
+
+            String listing = "LIST";
+            String adding = "ADD";
+            String addOnIndex = "ADD\\s+\\d+";
+            String editing = "EDIT\\s+\\d+";
+            String deleting = "DELETE\\s+\\d+";
+
+            if (ss.matches(listing)) {
+                todoList.showList();
+            }
+            if (ss.matches(adding)) {
+                System.out.println("Сделайте запись : ");
+                todoList.add(scanner.nextLine());
+            }
+            if (ss.matches(addOnIndex)) {      // выковыриваем цифру индекса из строки
+                String[] arr = ss.split(" ");
+                int num = Integer.parseInt(arr[1]);
+
+                System.out.println("Сделайте запись (индекс уже определен) : ");
+                todoList.insert(num, scanner.nextLine());
+            }
+            if (ss.matches(editing)) {
+                if (todoList.getSchedule().size() == 0) {
+                    System.out.println("Лист пустой");
+                }
+                String[] arr = ss.split(" ");
+                int num = Integer.parseInt(arr[1]);
+                System.out.println("Замените пункт (индекс уже определен) : ");
+                todoList.edit(num, scanner.nextLine());
+            }
+            if (ss.matches(deleting)) {
+                String[] arr = ss.split(" ");
+                int num = Integer.parseInt(arr[1]);
+                if (num < 1) {
+                    num = 0;
+                }
+                System.out.println("Удаляем пункт (индекс уже определен) :");
+                todoList.delete(num);
+            }
+        }
+    }
 }
+
 
 
 
